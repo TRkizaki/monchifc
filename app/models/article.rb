89bde733765d1501260f1a7d425354f3adbbc74c
TR Kizaki <trkizaki.web@gmail.com>
2023-07-16 19:42:34 +0400

    using concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
+
+
 
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 end
+
