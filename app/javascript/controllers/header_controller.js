e7eb17d04eeac31102bf88c76af06165ed73c371
TR Kizaki <trkizaki.web@gmail.com>
2023-07-27 18:54:42 +0400

    setting el-transition on header

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from 'el-transition'
 
 export default class extends Controller {
+  static targets = ['openUserMenu'];
+
   connect() {
-    console.log('enter:', enter);
-    console.log('leave:', leave);
-    console.log('toggle:', toggle);
+    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+  }
+
+  toggleDropdownMenu(){
+   toggle(document.getElementById('menu-dropdown-items'));
   }
 }
