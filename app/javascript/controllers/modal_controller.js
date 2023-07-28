9067293829ffb52bcba59379d1ae420b7c8cc96b
TR Kizaki <trkizaki.web@gmail.com>
2023-07-28 11:48:32 +0400

    edited modal controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,5 +6,18 @@ export default class extends Controller {
      enter(document.getElementById('modal-wrapper'));
      enter(document.getElementById('modal-backdrop'));
      enter(document.getElementById('modal-panel'));
+
+     document.getElementById('modal-wrapper').addEventListener('click', this.closeModal);
+   }
+
+   closeModal(event) {
+    const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
+
+    if(!modalPanelClicked) {
+     leave(document.getElementById('modal-wrapper'));
+     leave(document.getElementById('modal-backdrop'));
+     leave(document.getElementById('modal-panel'));
+
+    }
    }
  }
