af92ff2b55b90b74fd738270fb28228273a9ee28
TR Kizaki <trkizaki.web@gmail.com>
2023-08-06 11:18:04 +0400

    setting up the users API

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Got some error in Rails on sprockets precompilation step after tailwind build file generated
+  config.assets.css_compressor = nil 
 end
